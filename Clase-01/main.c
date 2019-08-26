291f1dd26f4ae3ad35185b5bbf05b22f3485ba17
alumno <alumno@utnfra.com.ar>
2019-08-26 12:07:40 -0300

    Tarea 3 y 4 completa

--- Clase-01/main.c
+++ Clase-01/main.c
@@ -7,10 +7,11 @@ int main(){
 int dinero;
 char tipo; //P, D, L, B
 int v;
-int dolarMax;
+int dolarMax = 0;
 int flag = 0;
 int acumP = 0;
 int contP = 0;
+float porP;
 float promP = 0;
 
     for(v = 0; v < 5; v++){
@@ -20,10 +21,22 @@ float promP = 0;
            fflush(stdin);
            scanf("%c",&tipo);
 
+           while(!(tipo == 'p' || tipo == 'd' || tipo == 'l' || tipo == 'b'))
+
+           {
+
+        printf("Ingrese un tipo valido.");
+
+           fflush(stdin);
+           scanf("%c",&tipo);
+
+           }
+
            printf("Ingrese cantidad de dinero.");
 
            fflush(stdin);
            scanf("%d",&dinero);
+
 
            if((dinero > dolarMax && tipo == 'd') || (flag == 0&& tipo == 'd')){
 
@@ -44,20 +57,21 @@ float promP = 0;
            }
 
 
-
-
-
-      }
+        }
 
       if(contP != 0){
 
-            promP = acumP / contP;
+            promP = acumP / (float)contP;
 
            }
 
+           porP = contP * 100 / v;
+
+
 
       printf("\nDolar Maximo: %d",dolarMax);
-      printf("\nPromedio de pesos; %f",promP);
+      printf("\nPromedio de pesos: %f",promP);
+      printf("\nPorcentaje de pesos:%f",porP);
 
       return 0;
 
